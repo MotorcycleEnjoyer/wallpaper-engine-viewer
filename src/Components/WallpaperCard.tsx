50525b2941ab227aa316da52461f750392138859
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2024-11-17 15:13:08 -0500

    tweak: make the preview cover entire wallpaper div, with a transparent header on the bottom

--- src/Components/WallpaperCard.tsx
+++ src/Components/WallpaperCard.tsx
@@ -5,20 +5,17 @@ export default function WallpaperCard(props: WallpaperInfoType) {
   const MAX_TITLE_LENGTH = 20
 
   return (
-    <div className="h-32 border border-transparent hover:cursor-pointer hover:border-[#24c8db;]">
-      <div className="h-3/4 overflow-hidden bg-blue-500">
-        <img src={preview} className="" alt="image" />
-      </div>
-      <div className="h-1/4 bg-black">
-        <h1>
+    <div className="h-32 border border-transparent hover:border-[#24c8db;]">
+      <div
+        className="flex h-full flex-col justify-end overflow-hidden bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${preview})` }}
+      >
+        <h1 className="bg-black bg-opacity-50 p-1">
           {props.title.length > MAX_TITLE_LENGTH
             ? `${props.title.substring(0, MAX_TITLE_LENGTH)}...`
             : props.title}
         </h1>
       </div>
-      {/* <h1>{title}</h1>
-        <img src={previewFilePath} alt="previewImg" /> */}
-      {/* <video src={previewFilePath}></video> */}
     </div>
   )
 }
--- src/Routes/Wallpapers.tsx
+++ src/Routes/Wallpapers.tsx
@@ -9,7 +9,7 @@ export default function Wallpapers() {
   }, [])
   return (
     <div className="flex-1">
-      <div className="grid max-h-full w-full grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10">
+      <div className="grid w-full grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10">
         {wallpapers.map((info) => (
           <WallpaperCard {...info} />
         ))}
